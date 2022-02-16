f38c65fb8422c4ec27c28f09c689bc58138c4d38
uruwanara <47467769+uruwanara@users.noreply.github.com>
2022-02-17 00:22:57 +0530

    Files Tree Organized

---
+++ Code/IceBreakerMiniProject/IceBreakerMiniProject/DAC/IBMPSOParts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PX.Data;
+namespace IceBreakerMiniProject
+{
+    [PXCacheName("SOParts")]
+    [PXPrimaryGraph(typeof(IBMPSalesOrderMaint))]
+    public class IBMPSOParts : IBMPSalesOrderLine
+    {
+        #region Partid
+        [PXDBInt(IsKey = true)]
+        [PXDefault(typeof(IBMPInventory.inventoryID))]
+        [PXUIField(DisplayName = "Inventory")]
+        [PXSelector(
+              typeof(
+                  Search<IBMPInventory.inventoryID,
+                      Where<IBMPInventory.inventoryType.IsEqual<Constant.stockItem>>>
+              ),
+              typeof(IBMPInventory.inventoryCD),
+              typeof(IBMPInventory.price)
+            //,SubstituteKey =typeof(IBMPInventory.inventoryCD),
+            //DescriptionField =typeof(IBMPInventory.description)
+
+          )]
+        public virtual int? Partid { get; set; }
+        public abstract class partid : PX.Data.BQL.BqlInt.Field<partid> { }
+        #endregion
+    }
+}
